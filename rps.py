0b936f395d49b413064bf63d784ba93ccad2fcb1
desp <supermariocrafter05@gmail.com>
2025-09-03 10:19:53 +0100

    rock paper scissors
    text-based hand game in python

--- rps.py
+++ rps.py
@@ -16,6 +16,7 @@ streakHolder = None # who holds the streak
 streak = 0
 draw = False # boolean for tied result
 win = "" # string value for printing who won
+error = False # determines whether user inputted incorrect rps choice
 update = "[UPDATE 0.3.1 ALPHA | 27/11/2024]: removed redundancies and added more comments in code"
 
 # definitions
@@ -68,7 +69,7 @@ def init_record():
 
 # main code
 
-# init
+# initialise
 print("welcome to rock paper scissors! see how you can stack up against a computer that plays completely randomly :)")
 print("you may use full form (rock, paper, scissors) or abbreviated form (r, p, s)! type 'update' in the game end")
 print("prompt to view the most recent update")
@@ -78,7 +79,7 @@ init_record()
 while True:
     
 # user makes choice
-    print("rock paper scissors shoot")
+    print("rock paper scissors shoot!")
     usr = input()
     if(usr in opt):
         if(usr == "r"):
@@ -88,80 +89,85 @@ while True:
         if(usr == "s"):
             usr = "scissors"
     if(usr not in fullopt):
+        error = True
         print("error: user listed invalid choice")
-        input()
+        print("skipping round...")
 
 # cpu makes random choice
-    cpu = rng.choice(fullopt)
-    print(cpu)
+    if(error = False):
+        cpu = rng.choice(fullopt)
+        print(cpu)
 
 # determine who wins
-    if(usr == "rock" and cpu == "scissors"):
-        win = "user"
-        game_end(usr)
-        userscore += 1
-        draw = False
-    if(usr == "rock" and cpu == "paper"):
-        win = "cpu"
-        game_end(cpu)
-        cpuscore += 1
-        draw = False
-    if(usr == "rock" and cpu == "rock"):
-        print("draw!")
-        draw = True
-        print(streakHolder, " is on a ", streak, " streak", sep="")
+    if(error = False):
+        if(usr == "rock" and cpu == "scissors"):
+            win = "user"
+            game_end(usr)
+            userscore += 1
+            draw = False
+        if(usr == "rock" and cpu == "paper"):
+            win = "cpu"
+            game_end(cpu)
+            cpuscore += 1
+            draw = False
+        if(usr == "rock" and cpu == "rock"):
+            print("draw!")
+            draw = True
+            print(streakHolder, " is on a ", streak, " streak", sep="")
 
-    if(usr == "paper" and cpu == "rock"):
-        win = "user"
-        game_end(usr)
-        userscore += 1
-        draw = False
-    if(usr == "paper" and cpu == "scissors"):
-        win = "cpu"
-        game_end(cpu)
-        cpuscore += 1
-        draw = False
-    if(usr == "paper" and cpu == "paper"):
-        print("draw!")
-        draw = True
-        print(streakHolder, " is on a ", streak, " streak", sep="")
+        if(usr == "paper" and cpu == "rock"):
+            win = "user"
+            game_end(usr)
+            userscore += 1
+            draw = False
+        if(usr == "paper" and cpu == "scissors"):
+            win = "cpu"
+            game_end(cpu)
+            cpuscore += 1
+            draw = False
+        if(usr == "paper" and cpu == "paper"):
+            print("draw!")
+            draw = True
+            print(streakHolder, " is on a ", streak, " streak", sep="")
 
-    if(usr == "scissors" and cpu == "paper"):
-        win = "user"
-        game_end(usr)
-        userscore += 1
-        draw = False
-    if(usr == "scissors" and cpu == "rock"):
-        win = "cpu"
-        game_end(cpu)
-        cpuscore += 1
-        draw = False
-    if(usr == "scissors" and cpu == "scissors"):
-        print("draw!")
-        draw = True
-        print(streakHolder, " is on a ", streak, " streak", sep="")
+        if(usr == "scissors" and cpu == "paper"):
+            win = "user"
+            game_end(usr)
+            userscore += 1
+            draw = False
+        if(usr == "scissors" and cpu == "rock"):
+            win = "cpu"
+            game_end(cpu)
+            cpuscore += 1
+            draw = False
+        if(usr == "scissors" and cpu == "scissors"):
+            print("draw!")
+            draw = True
+            print(streakHolder, " is on a ", streak, " streak", sep="")
 
 # print results
-    record()
-    print()
-    print("user score: ", userscore, ", cpu score: ", cpuscore, sep="")
-    print("play again? (type quit to stop; update to view most recent update; clear to clear rpsRecords.txt)")
-    ans = input()
-    if(ans == "quit"):
-        quit()
-    if(ans == "update"):
-        print(update)
-    if(ans == "clear"):
-        with open("rpsRecords.txt", "w") as file:
-            file.write("--- CLEARED ---\n")
-    if(ans == "mario"):
-        print("we love mario :D")
-        userscore += 1
-        try:
-            with open("rpsRecords.txt", "a") as file:
-                file.write("mario :D\n")
-                file.write("+1 user score\n")
-                file.write("\n")
-        except FileNotFoundError:
-            input("CRITICAL ERROR: 'rpsRecords.txt' not found. shutting down... ")
-            exit()
+    if(error = False):
+        record()
+        print()
+        print("user score: ", userscore, ", cpu score: ", cpuscore, sep="")
+        print("play again? (type quit to stop; update to view most recent update; clear to clear rpsRecords.txt)")
+        ans = input()
+        if(ans == "quit"):
+            quit()
+        if(ans == "update"):
+            print(update)
+        if(ans == "clear"):
+            with open("rpsRecords.txt", "w") as file:
+                file.write("--- CLEARED ---\n")
+        if(ans == "mario"):
+            print("we love mario :D")
+            userscore += 1
+            try:
+                with open("rpsRecords.txt", "a") as file:
+                    file.write("mario :D\n")
+                    file.write("+1 user score\n")
+                    file.write("\n")
+            except FileNotFoundError:
+                input("CRITICAL ERROR: 'rpsRecords.txt' not found. shutting down... ")
+                exit()
+
