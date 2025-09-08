d64ca50e05fb96ecc5423562c098bd178f60d73e
desp <supermariocrafter05@gmail.com>
2025-09-08 11:22:34 +0100

    Update intro.cpp

--- intro.cpp
+++ intro.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
-  cout << "Hello, World!" << endl;
+  char words[4] = {"Hello" ", " "World", "! "};
+  cout << words[1], words[3], words[0], words[2] << endl;
   return 0;
 }
